b6f7c2ec8d9ae4a34cf24d9cfc3234c5231b3e79
moulaalis808-cpu <moulaalis808@gmail.com>
2026-04-30 08:09:57 +0530

    Add files via upload

---
+++ prob3.py
@@ -0,0 +1,7 @@
+pets = ['cat','dog','rat','tiger','lion']
+snakes = ['viper','anaconda','cobra','king cobra']
+print("Snakes are:",snakes)
+animals = pets+snakes
+print("Animals are:",animals)
+snakes.remove("cobra")
+print("Updated Snakes are:",snakes)
